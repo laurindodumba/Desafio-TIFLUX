3b5154bb99f0c7c4473f924b25e25c35e3510832
https://github.com/laurindodumba/An-lise-de-Faturamento.py <dumbalvd@gmail.com>
2021-11-09 16:52:14 -0300

    Criado o Site Bancário para o desafio da TIFFLUX

---
+++ script.js
@@ -0,0 +1,20 @@
+
+this.listaTabela();
+
+
+
+listaTabela(); {
+    let tbody = document.getElementById('tbody');
+    let tr = tbody.insertRow();
+    let td_nome = tr.insertCell();
+    let td_descricao = tr.insertCell();
+    let td_criacao = insertCell();
+    let td_atualizacao = insertCell();
+    let td_votacao = insertCell();
+}
+
+
+
+
+
+
